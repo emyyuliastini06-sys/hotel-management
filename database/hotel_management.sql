d9f06f667ee745e45dd26a8ae7a824e4b536fcc1
emyyuliastini06-sys <emyyuliastini06@gmail.com>
2026-06-30 00:36:47 +0800

    menambahkan database hotel; hotel_management.sql

---
+++ database/hotel_management.sql
@@ -0,0 +1,228 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 29, 2026 at 06:33 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.0.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `hotel_management`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kamar`
+--
+
+CREATE TABLE `kamar` (
+  `id_kamar` int(11) NOT NULL,
+  `nomor_kamar` varchar(10) NOT NULL,
+  `id_tipe` int(11) NOT NULL,
+  `status` enum('Tersedia','Terisi','Maintenance') DEFAULT 'Tersedia'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pembayaran`
+--
+
+CREATE TABLE `pembayaran` (
+  `id_pembayaran` int(11) NOT NULL,
+  `id_pemesanan` int(11) NOT NULL,
+  `tanggal_bayar` date DEFAULT NULL,
+  `total_bayar` decimal(12,2) DEFAULT NULL,
+  `metode_pembayaran` enum('Cash','Transfer','QRIS') DEFAULT NULL,
+  `status` enum('Lunas','Belum Lunas') DEFAULT 'Belum Lunas'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pemesanan`
+--
+
+CREATE TABLE `pemesanan` (
+  `id_pemesanan` int(11) NOT NULL,
+  `id_tamu` int(11) NOT NULL,
+  `id_kamar` int(11) NOT NULL,
+  `tanggal_checkin` date DEFAULT NULL,
+  `tanggal_checkout` date DEFAULT NULL,
+  `jumlah_tamu` int(11) DEFAULT NULL,
+  `status` enum('Booking','Check In','Check Out') DEFAULT 'Booking'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tamu`
+--
+
+CREATE TABLE `tamu` (
+  `id_tamu` int(11) NOT NULL,
+  `nik` varchar(20) DEFAULT NULL,
+  `nama_tamu` varchar(100) NOT NULL,
+  `jenis_kelamin` enum('L','P') DEFAULT NULL,
+  `no_hp` varchar(20) DEFAULT NULL,
+  `alamat` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tipe_kamar`
+--
+
+CREATE TABLE `tipe_kamar` (
+  `id_tipe` int(11) NOT NULL,
+  `nama_tipe` varchar(100) NOT NULL,
+  `harga` decimal(12,2) NOT NULL,
+  `kapasitas` int(11) NOT NULL,
+  `fasilitas` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id_user` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `role` enum('admin','resepsionis','tamu') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id_user`, `username`, `password`, `role`) VALUES
+(1, 'admin', '0192023a7bbd73250516f069df18b500', 'admin');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `kamar`
+--
+ALTER TABLE `kamar`
+  ADD PRIMARY KEY (`id_kamar`),
+  ADD KEY `id_tipe` (`id_tipe`);
+
+--
+-- Indexes for table `pembayaran`
+--
+ALTER TABLE `pembayaran`
+  ADD PRIMARY KEY (`id_pembayaran`),
+  ADD KEY `id_pemesanan` (`id_pemesanan`);
+
+--
+-- Indexes for table `pemesanan`
+--
+ALTER TABLE `pemesanan`
+  ADD PRIMARY KEY (`id_pemesanan`),
+  ADD KEY `id_tamu` (`id_tamu`),
+  ADD KEY `id_kamar` (`id_kamar`);
+
+--
+-- Indexes for table `tamu`
+--
+ALTER TABLE `tamu`
+  ADD PRIMARY KEY (`id_tamu`);
+
+--
+-- Indexes for table `tipe_kamar`
+--
+ALTER TABLE `tipe_kamar`
+  ADD PRIMARY KEY (`id_tipe`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id_user`),
+  ADD UNIQUE KEY `username` (`username`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `kamar`
+--
+ALTER TABLE `kamar`
+  MODIFY `id_kamar` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `pembayaran`
+--
+ALTER TABLE `pembayaran`
+  MODIFY `id_pembayaran` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `pemesanan`
+--
+ALTER TABLE `pemesanan`
+  MODIFY `id_pemesanan` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tamu`
+--
+ALTER TABLE `tamu`
+  MODIFY `id_tamu` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tipe_kamar`
+--
+ALTER TABLE `tipe_kamar`
+  MODIFY `id_tipe` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `kamar`
+--
+ALTER TABLE `kamar`
+  ADD CONSTRAINT `kamar_ibfk_1` FOREIGN KEY (`id_tipe`) REFERENCES `tipe_kamar` (`id_tipe`);
+
+--
+-- Constraints for table `pembayaran`
+--
+ALTER TABLE `pembayaran`
+  ADD CONSTRAINT `pembayaran_ibfk_1` FOREIGN KEY (`id_pemesanan`) REFERENCES `pemesanan` (`id_pemesanan`);
+
+--
+-- Constraints for table `pemesanan`
+--
+ALTER TABLE `pemesanan`
+  ADD CONSTRAINT `pemesanan_ibfk_1` FOREIGN KEY (`id_tamu`) REFERENCES `tamu` (`id_tamu`),
+  ADD CONSTRAINT `pemesanan_ibfk_2` FOREIGN KEY (`id_kamar`) REFERENCES `kamar` (`id_kamar`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
